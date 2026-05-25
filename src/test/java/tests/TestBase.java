7aef871c87c09bd4ed4a7d8f9a4ba867a5aefa79
marushana <marushana@yandex.ru>
2026-05-25 20:35:11 +0300

    testNGListener

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,14 +1,13 @@
 package tests;
 
 import manager.ApplicationManager;
+import manager.TestNGListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
+@Listeners(TestNGListener.class)//to connect listener fron testNG
 
 public class TestBase {
     Logger logger = LoggerFactory.getLogger(TestBase.class);//org.slf4j
