dd1fa225912efc30b7384eda1d1fcdc22b7e40ec
marushana <marushana@yandex.ru>
2026-05-25 17:20:46 +0300

    remove contacts

--- src/test/java/tests/RemoveContactTests.java
+++ src/test/java/tests/RemoveContactTests.java
@@ -2,15 +2,17 @@ package tests;
 
 import model.User;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class RemoveContactTests extends TestBase{
-    @BeforeClass
+    @BeforeMethod
     public void preCondition(){
         if (!app.getHelperUser().isLogged()){
             app.getHelperUser().login(new User().setEmail("marushana@yandex.ru").setPassword("Pokrov1304!"));
 
         }
+        //app.getHelperContact().provideContacts();//if List of contacts less then 3 then add 3 contacts
     }
 
     @Test
